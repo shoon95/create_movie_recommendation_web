57f5668798e196549f8f48ac93905aa8b1c52837
edu-sanghoon <tkdgns5817@gmail.com>
2022-05-17 21:29:03 +0900

    settings.py

--- back/Mozul/settings.py
+++ back/Mozul/settings.py
@@ -125,6 +125,7 @@ STATIC_URL = '/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
+# 여기가 수정
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
