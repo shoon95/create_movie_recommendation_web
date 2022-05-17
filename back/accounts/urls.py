a52f7411ea5a64075c4ce7bc79f0d7474af38196
cih831 <choi83195@gmail.com>
2022-05-17 21:23:26 +0900

    test1

--- back/accounts/urls.py
+++ back/accounts/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # path('articles/<int:article_pk>/comments/', views.comment_create),
     # path('comments/', views.comment_list),
     # path('comments/<int:comment_pk>/', views.comment_detail),
+    # testtest 이건 테스트야!!
 ]
