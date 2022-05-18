02d7c68a88b91c35c9016ec55e57cc45bd62886e
edu-sanghoon <tkdgns5817@gmail.com>
2022-05-19 00:38:14 +0900

    front

--- front/final/src/router/index.js
+++ front/final/src/router/index.js
@@ -1,7 +1,7 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import HomeView from '../views/HomeView.vue'
-
+import LoginView from '../views/LoginView.vue'
 Vue.use(VueRouter)
 
 const routes = [
@@ -9,6 +9,11 @@ const routes = [
     path: '/',
     name: 'home',
     component: HomeView
+  },
+  {
+    path:'/login',
+    name: 'login',
+    component: LoginView
   },
 ]
 
---
+++ front/final/src/views/LoginView.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <input type="text"><br>
+    <input class='mt-2 mb-2' type="text"><br>
+    <button class="btn btn-primary">로그인</button>
+  </div>
+</template>
+
+<script>
+
+
+export default {
+  name: 'HomeView',
+  components: {
+
+  }
+  
+  
+  
+}
+
+
+</script>
+
+<style scoped>
+  
+</style>
