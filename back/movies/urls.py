6b1bff30e28f09641f243ba7b4442fdff16e6d0f
edu-sanghoon <tkdgns5817@gmail.com>
2022-05-19 00:42:49 +0900

    r

--- back/movies/urls.py
+++ back/movies/urls.py
@@ -4,6 +4,6 @@ from . import views
 urlpatterns = [
     # path('actors/', views.actor_list),
     # path('actors/<int:actor_pk>/', views.actor_detail),
-    path('', views.movie_list),
-    path('<int:movie_pk>/', views.movie_detail),
+    # path('', views.movie_list),
+    # path('<int:movie_pk>/', views.movie_detail),
 ]
