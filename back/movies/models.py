6fb01d9a014791759a88097f18226903741d17df
edu-sanghoon <tkdgns5817@gmail.com>
2022-05-19 00:56:09 +0900

    video

--- back/movies/models.py
+++ back/movies/models.py
@@ -4,10 +4,10 @@ from django.conf import settings
 class Genre(models.Model):
     name = models.CharField(max_length=50)
 
-class Video(models.Model):
+class Actor(models.Model):
+    name = models.CharField(max_length=100)
+    profile_path = models.CharField(max_length=200)
     
-    youtube_key = models.TextField()
-
 class Movie(models.Model):
     title = models.CharField(max_length=100)
     release_date = models.DateField()
@@ -17,9 +17,13 @@ class Movie(models.Model):
     overview = models.TextField()
     poster_path = models.CharField(max_length=200)
     genres = models.ManyToManyField(Genre)
-    video = models.ForeignKey(Video, on_delete=models.CASCADE)
     like_users = models.ManyToManyField(settings.AUTH_USER_MODEL, related_name='like_movies')
+    actors = models.ManyToManyField(Actor)
 
     def __str__(self):
         return self.title
+
+class Video(models.Model):
+    movie = models.ForeignKey(Movie, models.CASCADE)
+    youtube_key = models.TextField()
 
